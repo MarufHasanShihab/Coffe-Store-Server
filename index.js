0e3c705c98f90b88efe7ae5077f5298024eda2f5
Maruf Hasan Shihab <devmarufhasan@gmail.com>
2024-12-12 00:35:46 +0600

    added cors permison

--- index.js
+++ index.js
@@ -7,6 +7,11 @@ const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors());
+app.use(cors({
+  origin: 'https://coffe-store-server-bbf2iayb8-maruf-hasan-shihabs-projects.vercel.app/', // Allow requests from this origin
+  methods: 'GET,POST,PUT,DELETE',
+  credentials: true // If using cookies or authentication
+}))
 app.use(express.json());
 
 
